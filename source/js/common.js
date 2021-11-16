14ea93e10148c13f1927d555241e743321b7fed1
romanshliakhov <romanshliakhov@gmail.com>
2021-11-16 21:48:39 +0200

    added input mask

--- source/js/common.js
+++ source/js/common.js
@@ -70,42 +70,41 @@ function validFileType(file) {
   return false;
 }
 
-// imput Mask
-window.addEventListener("DOMContentLoaded", function () {
-  [].forEach.call(document.querySelectorAll('.tel'), function (input) {
-    var keyCode;
+// window.addEventListener("DOMContentLoaded", function () {
+//   [].forEach.call(document.querySelectorAll('.tel'), function (input) {
+//     var keyCode;
 
-    function mask(event) {
-      event.keyCode && (keyCode = event.keyCode);
-      var pos = this.selectionStart;
-      if (pos < 3) event.preventDefault();
-      var matrix = "+ _ (___) ___-__-__",
-        i = 0,
-        def = matrix.replace(/\D/g, ""),
-        val = this.value.replace(/\D/g, ""),
-        new_value = matrix.replace(/[_\d]/g, function (a) {
-          return i < val.length ? val.charAt(i++) || def.charAt(i) : a
-        });
-      i = new_value.indexOf("_");
-      if (i != -1) {
-        i < 5 && (i = 3);
-        new_value = new_value.slice(0, i)
-      }
-      var reg = matrix.substr(0, this.value.length).replace(/_+/g,
-        function (a) {
-          return "\\d{1," + a.length + "}"
-        }).replace(/[+()]/g, "\\$&");
-      reg = new RegExp("^" + reg + "$");
-      if (!reg.test(this.value) || this.value.length < 5 || keyCode > 47 && keyCode < 58) this.value = new_value;
-      if (event.type == "blur" && this.value.length < 5) this.value = "";
-    }
+//     function mask(event) {
+//       event.keyCode && (keyCode = event.keyCode);
+//       var pos = this.selectionStart;
+//       if (pos < 3) event.preventDefault();
+//       var matrix = "+ _ (___) ___-__-__",
+//         i = 0,
+//         def = matrix.replace(/\D/g, ""),
+//         val = this.value.replace(/\D/g, ""),
+//         new_value = matrix.replace(/[_\d]/g, function (a) {
+//           return i < val.length ? val.charAt(i++) || def.charAt(i) : a
+//         });
+//       i = new_value.indexOf("_");
+//       if (i != -1) {
+//         i < 5 && (i = 3);
+//         new_value = new_value.slice(0, i)
+//       }
+//       var reg = matrix.substr(0, this.value.length).replace(/_+/g,
+//         function (a) {
+//           return "\\d{1," + a.length + "}"
+//         }).replace(/[+()]/g, "\\$&");
+//       reg = new RegExp("^" + reg + "$");
+//       if (!reg.test(this.value) || this.value.length < 5 || keyCode > 47 && keyCode < 58) this.value = new_value;
+//       if (event.type == "blur" && this.value.length < 5) this.value = "";
+//     }
 
-    input.addEventListener("input", mask, false);
-    input.addEventListener("focus", mask, false);
-    input.addEventListener("blur", mask, false);
-    input.addEventListener("keydown", mask, false);
-  });
-});
+//     input.addEventListener("input", mask, false);
+//     input.addEventListener("focus", mask, false);
+//     input.addEventListener("blur", mask, false);
+//     input.addEventListener("keydown", mask, false);
+//   });
+// });
 
 // burger
 let menuBtn = document.querySelector('.burger-menu');
@@ -306,3 +305,10 @@ const modal = new Modal({
 
   },
 });
+
+// imput Mask
+let selector = document.querySelectorAll('input[type="tel"]');
+
+let im = new Inputmask('+9 (999) 999-99-99');
+
+im.mask(selector);
